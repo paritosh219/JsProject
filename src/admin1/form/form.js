1414782aa413116e636c8f09d270ea71c948e7a9
Paritosh Saini <154421461+paritosh219@users.noreply.github.com>
2025-04-21 23:57:30 +0530

    Delete src/admin1 directory

--- src/admin1/form/form.js
+++
@@ -1,6 +0,0 @@
-const form = document.getElementById("cityForm");
-const city = document.getElementById("city");
-const newCity = document.getElementById("newCity");
-const add = document.getElementById("addBtn");
-const update = document.getElementById("updateBtn");
-const dlt = document.getElementById("deleteBtn");
